45861afbd51411036a74de6f5dea84b6cf3ac75a
Zexx309 <162204079+Zexx309@users.noreply.github.com>
2024-03-31 09:10:21 +0530

    Update stream.py
    Signed-off-by: Zexx309 <162204079+Zexx309@users.noreply.github.com>

--- AnonXMusic/utils/stream/stream.py
+++ AnonXMusic/utils/stream/stream.py
@@ -11,7 +11,7 @@ from AnonXMusic.misc import db
 from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
 from AnonXMusic.utils.exceptions import AssistantErr
 from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import DilBin
+from AnonXMusic.utils.pastebin import AnonyBin
 from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
 from AnonXMusic.utils.thumbnails import get_thumb
 
